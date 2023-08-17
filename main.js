a2a7a28849e60dfb03ca21531040cef5aa2dd7d6
GoodGuyNaiv <naivedhyap@gmail.com>
2023-08-17 19:23:22 +0530

    functionality working

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
 
-function playSound(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio)return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add("playing")
-    
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
 }
-function removeTransition(e){
-    
-    this.classList.remove('playing');
+function removeTransition(e) {
+  this.classList.remove("playing");
 }
-window.addEventListener('keydown', playSound)
+window.addEventListener("keydown", playSound);
 const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener('transitonend', removeTransition));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
