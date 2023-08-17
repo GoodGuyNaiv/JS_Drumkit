4716cb7f438fa7ae8e1c5373e32633d0482b6d44
GoodGuyNaiv <naivedhyap@gmail.com>
2023-08-17 19:24:38 +0530

    replace key with key-code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
   if (!audio) return;
   audio.currentTime = 0;
